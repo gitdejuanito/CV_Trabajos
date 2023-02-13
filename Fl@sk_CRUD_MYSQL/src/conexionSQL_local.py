eeffb0dab9134a17690875505501759185de90b7
gitdejuanito <arana.juandedios@gmail.com>
2023-02-13 01:21:59 -0600

    14 commit

--- Fl@sk_CRUD_MYSQL/src/conexionSQL_local.py
+++ Fl@sk_CRUD_MYSQL/src/conexionSQL_local.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-conexion = mysql.connector.connect(user="root", password="Chelsea@01", host="localhost", 
+conexion = mysql.connector.connect(user="root", password="", host="localhost", 
 database= "sql_store", port="3306")
 
 print (conexion)
